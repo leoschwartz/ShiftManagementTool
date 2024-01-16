6c7e8f0fe91fcc9eee8bc0c44485d687a20bab28
Anwyll Le <lelinhnguyen2109@gmail.com>
2024-01-16 09:26:59 -0500

    added theme to application

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <>
-      <h1 className='text-xl font-bold'>Tailwind test</h1>
+      <h1 className="text-xl font-bold text-fifth">Tailwind test</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
