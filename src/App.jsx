ac4664c9a41c5ae4e8426fdd0365767e0e547fcd
Anwyll Le <lelinhnguyen2109@gmail.com>
2024-01-16 10:37:43 -0500

    created  a basic route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import "./App.css";
 import Homepage from "./routes/Homepage";
-
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <Homepage />
+      <Router>
+        <Routes>
+          <Route
+            path="*"
+            element={
+              <>
+                <Navbar />
+                <Homepage />
+              </>
+            }
+          ></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
