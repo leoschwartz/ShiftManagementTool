c4a3cb8ca6424138c6a90c1e538b16ffc6151873
Anwyll Le <lelinhnguyen2109@gmail.com>
2024-01-27 01:48:25 -0500

    removed home page, added error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,147 +1,27 @@
 import "./App.css";
 import SchedulePage from "./routes/SchedulePage";
-import Homepage from "./routes/Homepage";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./routes/Login";
 import Profile from "./routes/Profile";
-import { useAtom } from "jotai";
-import { isLoggedInAtom } from "./globalAtom";
 import ProtectedRoute from "./components/ProtectedRoute";
+import Error404 from "./routes/Error404";
 function App() {
-  const [isLoggedIn] = useAtom(isLoggedInAtom);
   return (
-    // <>
-    //   <Router>
-    //     <Routes>
-    //       <Route
-    //         path="/login"
-    //         element={
-    //           <>
-    //             <Navbar />
-    //             <Login />
-    //           </>
-    //         }
-    //       ></Route>
-    //       <Route
-    //         path="*"
-    //         element={
-    //           <>
-    //             <Navbar />
-    //             {isLoggedIn ? <Homepage /> : <Login />}
-    //           </>
-    //         }
-    //       ></Route>
-    //       <Route
-    //         path="/homepage"
-    //         element={
-    //           <>
-    //             <Navbar />
-    //             <Homepage />
-    //           </>
-    //         }
-    //       ></Route>
-    //       <Route
-    //         path="/profile"
-    //         element={
-    //           <>
-    //             <Navbar />
-    //             <Profile />
-    //           </>
-    //         }
-    //       ></Route>
-    //       <Route
-    //         path="/schedule"
-    //         element={
-    //           <>
-    //             <Navbar />
-    //             <SchedulePage />
-    //           </>
-    //         }
-    //       ></Route>
-    //     </Routes>
-    //   </Router>
-    // </>
     <>
       <Router>
         <Navbar />
         <Routes>
-          <Route
-            path="/login"
-            element={
-              <>
-                <Login />
-              </>
-            }
-          ></Route>
-          <Route
-            path="/homepage"
-            element={
-              <>
-                <Homepage />
-              </>
-            }
-          ></Route>
-          <Route
-            path="/profile"
-            element={
-              <>
-                <Profile />
-              </>
-            }
-          ></Route>
-          <Route
-            path="/schedule"
-            element={
-              <>
-                <SchedulePage />
-              </>
-            }
-          ></Route>
-          <Route
-            path="*"
-            element={<>{isLoggedIn ? <Homepage /> : <Login />}</>}
-          ></Route>
-          {/* for testing */}
-          <Route path="landing" element={<Landing />} />
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+
           <Route element={<ProtectedRoute />}>
-            <Route path="home" element={<Home />} />
-            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="/schedule" element={<SchedulePage />} />
           </Route>
-          <Route path="analytics" element={<Analytics />} />
-          <Route path="admin" element={<Admin />} />
-          <Route path="*" element={<p>There&apos;s nothing here: 404!</p>} />
-          {/* delete above */}
+          <Route path="*" element={<Error404 />}></Route>
         </Routes>
       </Router>
     </>
   );
 }
-
-// for testing
-const Landing = () => {
-  return <h2>Landing (Public: anyone can access this page)</h2>;
-};
-
-const Home = () => {
-  return <h2>Home (Protected: authenticated user required)</h2>;
-};
-
-const Dashboard = () => {
-  return <h2>Dashboard (Protected: authenticated user required)</h2>;
-};
-
-const Analytics = () => {
-  return (
-    <h2>
-      Analytics (Protected: authenticated user with permission analyze required)
-    </h2>
-  );
-};
-
-const Admin = () => {
-  return (
-    <h2>Admin (Protected: authenticated user with role admin required)</h2>
-  );
-};
 export default App;
---
+++ src/routes/Error404.jsx
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom";
+import { useAtom } from "jotai";
+import { isLoggedInAtom } from "../globalAtom";
+function Error404() {
+  const [isLoggedIn] = useAtom(isLoggedInAtom);
+  return (
+    <section className="relative" id="home">
+      {/* Background design */}
+      <div
+        aria-hidden="true"
+        className="absolute inset-0 grid grid-cols-2 -space-x-52"
+      >
+        <div className="blur-[106px] h-1/3 bg-gradient-to-br from-primary to-secondary opacity-50 "></div>
+        <div className="blur-[106px] h-3/4 bg-gradient-to-r from-forth to-fifth opacity-30"></div>
+      </div>
+      {/* Content */}
+      <div className="max-w-7xl mx-auto px-6 md:px-12 xl:px-6">
+        <div className="relative pt-36 ml-auto">
+          <div className="lg:w-2/3 text-center mx-auto">
+            <h1 className="text-forth  font-bold text-4xl md:text-5xl xl:text-6xl">
+              404
+            </h1>
+            <p className="mt-8 text-secondary font-semibold">
+              Look like you&apos;re lost, let&apos;s get you back home
+            </p>
+            <div className="mt-8 flex flex-wrap justify-center gap-y-4 gap-x-6">
+              {isLoggedIn ? (
+                <Link
+                  to="/schedule"
+                  className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:border before:border-transparent before:bg-fifth before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
+                >
+                  <span className="relative text-base font-semibold text-primary ">
+                    Go back to your dashboard
+                  </span>
+                </Link>
+              ) : (
+                <Link
+                  to="/login"
+                  className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:border before:border-fifth before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
+                >
+                  <span className="relative text-base font-semibold text-primary ">
+                    Log in
+                  </span>
+                </Link>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Error404;
