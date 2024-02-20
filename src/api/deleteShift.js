96b071caaf387210e7ffbc3ba83abfcc29d7b22f
Anwyll Le <lelinhnguyen2109@gmail.com>
2024-02-20 07:19:36 -0500

    updated some routes

--- src/api/deleteShift.js
+++ src/api/deleteShift.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const deleteShift = async (userToken, shiftId) => {
-  //   const apiUrl = import.meta.env.VITE_API_URL + "/shift/delete";
-  const apiUrl = "http://localhost:5000/shifts/delete/" + shiftId;
+  const apiUrl = import.meta.env.VITE_API_URL + "/shift/delete" + shiftId;
+  // const apiUrl = "http://localhost:5000/shifts/delete/" + shiftId;
   if (!apiUrl) {
     throw new Error("API_URL is not defined");
   }
