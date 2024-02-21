6d1755511677a90324fb24dda289609229da68f7
Leonardo Schwartz da Costa Silva <lschwartz-da-costa-s@myseneca.ca>
2024-02-20 20:12:20 -0500

    Notification update and category spinner introduced

---
+++ src/api/getCategories.js
@@ -0,0 +1,23 @@
+const getCategories = async (userId, userToken) => {
+    try {
+      const apiUrl = import.meta.env.VITE_API_URL + `/manager/categoryList/${userId}`;
+      const response = await fetch(apiUrl, {
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        return data;
+      } else {
+        console.error("Could not get category list");
+        return [];
+      }
+    } catch (error) {
+      console.error("Error getting category list:", error);
+      return [];
+    }
+  };
+  
+  export default getCategories;
+  
---
+++ src/common/Shift.ts
@@ -0,0 +1,47 @@
+import {getShifts} from "../api/getShifts"
+import {getShift} from "../api/getShift"
+//The primary reason behind this intermediary object is to use it when editing gets implemented. 
+//This will be used to hold staged changes clientside.
+export class Shift {
+    constructor(id, name, startTime, endTime, assigner, desc, completed) {
+        this.id = id;
+        this.name = name;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.assigner = assigner;
+        this.desc = desc;
+        this.completed = completed;
+    }
+    id: number;
+    name: string;
+    startTime: Date;
+    endTime: Date;
+    assigner: String;
+    desc: String;
+    completed: boolean; 
+}
+//Convert shift object to FullCalendar event
+export function shiftToEvent(shift) {
+    return {
+        title: shift.name,
+        start: shift.startTime,
+        end : shift.endTime,
+        extendedProps: {
+            eventId: shift.id
+        },
+    };
+}
+//Retrieve FullCalendar events
+export async function getEvents(user, fetchInfo) {
+    const events : Object[] = [];
+    (await getShifts(user,fetchInfo)).map((x) => { 
+        events.push(shiftToEvent(x));
+    });
+    return events;
+}
+export async function getEvent(id) {
+    const returned = await getShift(id);
+    if (!returned)
+        return null;
+    return shiftToEvent(returned);
+}
