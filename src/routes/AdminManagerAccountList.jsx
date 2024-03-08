afc3e0cc063b22000039ef41e10ce4fcb6d9faec
Anwyll Le <lelinhnguyen2109@gmail.com>
2024-03-07 22:15:01 -0500

    temporary PR for Peter

--- src/routes/AdminManagerAccountList.jsx
+++ src/routes/AdminManagerAccountList.jsx
@@ -3,6 +3,7 @@ import { getAllAdminsAndManagers } from "../api/getAllAdminsAndManagers";
 import { useEffect, useState } from "react";
 import { useAtom } from "jotai";
 import { userTokenAtom } from "../globalAtom";
+import { Table } from "flowbite-react";
 function AdminManagerAccountList() {
   const [userToken] = useAtom(userTokenAtom);
   const [managerList, setManagerList] = useState([]);
@@ -23,9 +24,35 @@ function AdminManagerAccountList() {
     <>
       <Theme2 />
       <section>
-        {/* see all */}
-        {/* See only managers */}
-        {/* See only admins */}
+        <Table hoverable>
+          <Table.Head>
+            <Table.HeadCell>Product name</Table.HeadCell>
+            <Table.HeadCell>Color</Table.HeadCell>
+            <Table.HeadCell>Category</Table.HeadCell>
+            <Table.HeadCell>Price</Table.HeadCell>
+            <Table.HeadCell>
+              <span className="sr-only">Edit</span>
+            </Table.HeadCell>
+          </Table.Head>
+          <Table.Body className="divide-y">
+            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                {'Apple MacBook Pro 17"'}
+              </Table.Cell>
+              <Table.Cell>Sliver</Table.Cell>
+              <Table.Cell>Laptop</Table.Cell>
+              <Table.Cell>$2999</Table.Cell>
+              <Table.Cell>
+                <a
+                  href="#"
+                  className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                >
+                  Edit
+                </a>
+              </Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
       </section>
     </>
   );
